c638da339a610d16a9b2cd526fcae7a8e575c7b4
dkiefner <pivi_dev@gmx.de>
2015-04-27 23:10:04 +0200

    add Fossets sample for normal TextViews

--- fossets-sample/src/main/java/de/thepivi/fossets/sample/MainActivity.java
+++ fossets-sample/src/main/java/de/thepivi/fossets/sample/MainActivity.java
@@ -3,12 +3,13 @@ package de.thepivi.fossets.sample;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import de.thepivi.fossets.Fossets;
 import de.thepivi.fossets.widget.TypefaceButton;
 import de.thepivi.fossets.widget.TypefaceEditText;
 import de.thepivi.fossets.widget.TypefaceTextView;
@@ -16,11 +17,10 @@ import de.thepivi.fossets.widget.TypefaceTextView;
 
 public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener {
 
-    private static final String TAG = "Fossets-Sample";
-
-    private TypefaceTextView mText;
-    private TypefaceEditText mEdit;
-    private TypefaceButton mButton;
+    private TextView text;
+    private TypefaceTextView tfText;
+    private TypefaceEditText tfEdit;
+    private TypefaceButton tfButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +31,10 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
     }
 
     private void initViews() {
-        mText = (TypefaceTextView) findViewById(R.id.text);
-        mEdit = (TypefaceEditText) findViewById(R.id.edit);
-        mButton = (TypefaceButton) findViewById(R.id.button);
+        text = (TextView) findViewById(R.id.text);
+        tfText = (TypefaceTextView) findViewById(R.id.tf_text);
+        tfEdit = (TypefaceEditText) findViewById(R.id.tf_edit);
+        tfButton = (TypefaceButton) findViewById(R.id.tf_button);
 
         Spinner mSpinner = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.fonts_array, android.R.layout.simple_spinner_item);
@@ -59,16 +60,20 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
 
     private void updateFont(String fontPath) {
         Context context = getBaseContext();
-        mText.setTypeface(context, fontPath);
-        mEdit.setTypeface(context, fontPath);
-        mButton.setTypeface(context, fontPath);
+
+        // using Fossets on normal TextView
+        Fossets.with(context).set(text, fontPath);
+
+        // using TypeFaceViews
+        tfText.setTypeface(context, fontPath);
+        tfEdit.setTypeface(context, fontPath);
+        tfButton.setTypeface(context, fontPath);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
         String fontPath = getFontPath(pos);
         updateFont(fontPath);
-        Log.d(TAG, "pos=" + pos + " fontPath=" + fontPath);
     }
 
     @Override
